f7e271b25032fab6d526b0611739aced18ad1792
Chris Sakai <cs.chris.sakai@gmail.com>
2018-07-27 23:00:13 -0400

    Create ConnectedItemDetailPage component

---
+++ app/containers/ConnectedItemDetailPage.jsx
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux';
+
+import { readySelector, detailItemSelector } from '../selectors/shop.selector';
+import { getItems, setDetailId } from '../reducers/shop';
+import ItemDetailPage from '../components/ItemDetailPage.jsx';
+
+const mapStateToProps = state => ({
+    ready: readySelector(state),
+    item: detailItemSelector(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+    getItems: () => dispatch(getItems()),
+    setDetailId: id => dispatch(setDetailId(id))
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(ItemDetailPage);
